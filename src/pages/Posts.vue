4ffc2e0cb98bce91f1505e67f771f7d2d1275143
Ricardo Gauto <ricardogauto11@gmail.com>
2018-06-18 11:41:54 -0300

    Consumiendo api

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,12 +1,30 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-  </q-page>
+  <q-list v-if="posts && posts.length">
+    <q-item v-for="(id, post) in posts" :key="id">
+      <p><strong>{{ post.title }}</strong></p>
+      <p>{{ post.body }}</p>
+    </q-item>
+  </q-list>
 </template>
 
 <script>
 export default {
   name: 'Posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+
+  created() {
+    this.$axios.get('http://jsonplaceholder.typicode.com/posts')
+    .then((response) => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
 }
 </script>
 
